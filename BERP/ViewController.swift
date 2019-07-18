d12eca22d6a99879716f2cc9dcbd42bab2226524
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-18 12:42:32 -0400

    [Tarlochan] Added Remember me code using User default

--- BERP/ViewController.swift
+++ BERP/ViewController.swift
@@ -11,12 +11,41 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var txtEmail: UITextField!
     
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var switchRememberMe: UISwitch!
     @IBOutlet weak var txtPassword: UITextField!
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func actionLogin(_ sender: Any)
+    {
+        
+    }
+    @IBAction func actionRegister(_ sender: Any)
+    {
+        
+    }
+    
+    // Remember Me Code -> User Default
+    private func getRememberMeValues()
+    {
+        let userDefault = UserDefaults.standard
+        
+        if let email = userDefault.string(forKey: "userEmail")
+        {
+            txtEmail.text = email
+            
+            if let pwd = userDefault.string(forKey: "userPassword")
+            {
+                txtPassword.text = pwd
+            }
+        }
+        switchRememberMe.setOn(true, animated: true)
+    }
 }
 
