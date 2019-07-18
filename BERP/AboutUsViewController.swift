2607e05b6a6fe8612be54b6e8be21cf308e99991
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-18 12:28:18 -0400

    [Tarlochan] About us file rewrite again due to push pull problem

--- BERP/AboutUsViewController.swift
+++ BERP/AboutUsViewController.swift
@@ -1,14 +1,15 @@
 //
-//  ContactUsViewController.swift
+//  AboutUsViewController.swift
 //  BERP
 //
-//  Created by admin on 2019-07-17.
+//  Created by admin on 2019-07-18.
 //  Copyright © 2019 Bikloo. All rights reserved.
 //
 
 import UIKit
 import WebKit
-class AboutUsViewController: UIViewController {
+class AboutUsViewController: UIViewController
+{
     @IBOutlet weak var wkWebView: WKWebView!
     var contactUsUrlString : String? // url for contact us
     
@@ -26,15 +27,16 @@ class AboutUsViewController: UIViewController {
         let urlRequest = URLRequest(url: url!)
         self.wkWebView.load(urlRequest)
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
