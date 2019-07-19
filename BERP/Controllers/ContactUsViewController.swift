ff1c78ac97fe5b62a05b820b730cf4aa4abe750d
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-19 15:04:29 -0400

    [Tarlochan] Made Contact us visual look

---
+++ BERP/Controllers/ContactUsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactUsViewController.swift
+//  BERP
+//
+//  Created by Das Tarlochan Preet Singh on 2019-07-19.
+//  Copyright © 2019 Bikloo. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
