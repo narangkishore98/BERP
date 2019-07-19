4ceaf7a77b5aa7ad2aed07e2be92fea829e597af
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-19 18:00:42 -0400

    [Tarlochan] Day commit

--- BERP/Controllers/SmsCallViewController.swift
+++ BERP/Controllers/SmsCallViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 import MessageUI
-class SmsCallViewController: UIViewController ,MFMessageComposeViewControllerDelegate{
+class SmsCallViewController: UIViewController ,MFMessageComposeViewControllerDelegate
+{
     
     
 
